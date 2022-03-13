06987b92c83906c5a3b0f8b82d40cbeec71a082e
Malia Pringle <malia.pringle@gmail.com>
2022-03-12 19:02:23 -0800

    simple function

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+def school_age_calculator(age,name):
+  if age < 5:
+    print("enoyoy", name, "is only", age)
+  elif age == 5:
+    print("enjoy school,",  name)
+  else:
+    print("they grow up so fast")
+
+school_age_calculator(3, "Thomas")
